0d5d42854ae23fe3eeb085d6d5793ed6638707ad
Seg Fault <segfaulted11@gmail.com>
2026-02-15 19:55:56 +0600

    adding and removing class from an html  element

--- js.js
+++ js.js
@@ -1,1 +1,8 @@
-console.log("JW")
+//adding a class
+let cls = document.getElementById("id1")
+cls.classList.add('cls3')
+console.log(cls.className)//cls1, cls2, cls3
+
+//removing a class
+cls.classList.remove('cls2')
+console.log(cls.className)//cls1, cls3
